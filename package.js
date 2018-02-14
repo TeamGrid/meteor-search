f6463a73c7f4eb378204fa8a55a1ccea670b65ea
Max Nowack <max@unsou.de>
2018-02-14 17:39:49 +0100

    coffeescript@2.0.0

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
   name: 'teamgrid:search',
   summary: 'fulltext-search over multiple collections',
-  version: '1.1.7'
+  version: '1.2.0'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.6.1');
   api.use([
-    'coffeescript',
+    'coffeescript@2.0.0',
     'underscore',
     'reactive-var',
     'check',
     'templating@1.0.0',
     'blaze@2.0.0',
     'dburles:mongo-collection-instances@0.3.3',
-    'maximum:package-base@1.1.2'
+    'maximum:package-base@1.2.0'
   ]);
 
   api.addFiles([
